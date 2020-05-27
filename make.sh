f7db232a7a7efe4d106d691eaf454a25f99c0fea
Peter Lohse <peter.lohse@fsd-web.de>
2020-05-27 19:24:26 +0200

    Readme added

--- make.sh
+++ make.sh
@@ -2,5 +2,5 @@
 rm -f files.tar
 7z a -ttar -mx=9 files.tar ./files/*
 rm -f dev.hanashi.wsc.twemoji-selector.tar
-7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-selector.tar ./* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.twemoji-selector.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
+7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-selector.tar ./* -x!acptemplates -x!files -x!doc -x!README.adoc -x!templates -x!dev.hanashi.wsc.twemoji-selector.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
 rm -f files.tar
