eccc74c5d44f1ac9da15b5743ea9547eda930cd2
Peter Lohse <peter.lohse@fsd-web.de>
2020-05-27 18:39:43 +0200

    Make Datei korrigiert

--- make.sh
+++ make.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 rm -f files.tar
 7z a -ttar -mx=9 files.tar ./files/*
-rm -f templates.tar
-7z a -ttar -mx=9 templates.tar ./templates/*
 rm -f dev.hanashi.wsc.twemoji-selector.tar
 7z a -ttar -mx=9 dev.hanashi.wsc.twemoji-selector.tar ./* -x!acptemplates -x!files -x!templates -x!dev.hanashi.wsc.twemoji-selector.tar -x!.git -x!.gitignore -x!make.bat -x!make.sh
 rm -f files.tar
-rm -f templates.tar
