c4ae71afdde7c7953cca9e5c1dc10db69fcb587a
Peter Lohse <github@hsrvmail.net>
2021-07-11 11:24:26 +0200

    Buildscript korrigiert

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!.git -x!.gitignore -x!make.sh -x!make.bat -x!.phpcs.xml -x!.github -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!doc -x!README.adoc
 7z a ${PACKAGE_NAME}.tar.gz ${PACKAGE_NAME}.tar
 rm -rf ${PACKAGE_NAME}.tar
 
